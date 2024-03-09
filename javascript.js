cf3bb754fcd39be65eb8fe6d0c4a356f9e1ba575
ianhouston <smooveslushie@gmail.com>
2024-03-09 10:39:30 -0600

    made calculator fully functional except for the backtracking buttons. Also optimized the event listeners for the buttons.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,16 @@
 const calculatorContainer = document.querySelector('body').querySelector('.calculatorContainer');
-const calculatorText = calculatorContainer.querySelector('.calculatorTextContainer').querySelector('.calculatorText');
-// ADDING METHODS FOR BUTTON EVENTS
+const calculatorTextContainer = calculatorContainer.querySelector('.calculatorTextContainer');
+const calculatorText = calculatorTextContainer.querySelector('.calculatorText');
+const calculatorResult = calculatorTextContainer.querySelector('.calculatorResult');
 
+const decimalLimit = 3;
 let number1 = "";
 let number2 = "";
 let operator = "";
+let decimal = false;
 let operated = false;
+
+// ADDING METHODS FOR BUTTON EVENTS
 
 const resetCalculatorVariables = () => {
     number1 = ""; 
@@ -15,16 +20,54 @@ const resetCalculatorVariables = () => {
 
 const updateText = () => {
     calculatorText.textContent = `${number1} ${operator} ${number2}`;
+    if (operated) {
+        calculatorResult.textContent = "";
+    }
 }
 
-function addDigitToNum(nextDigit, number) {
-    if (number == 1) {
-        number1 = number1 + nextDigit;
-        return number1;
+function giveCurrentNumber() {
+    if (operator == "" || operated == true) {
+        if (operated == true) {
+            resetCalculatorVariables();
+        }
+        return 1;
     }
     else {
-        number2 = number2 + nextDigit;
-        return number2;
+        return 2;
+    }
+}
+
+function addToNum(nextDigit, number) {
+    if (number == 1) {
+        number1+= nextDigit;
+    }
+    else {
+        number2+= nextDigit;
+    }
+}
+
+const changeNumberSign = () => {
+    if (giveCurrentNumber() == 1) {
+        if (number1 == "") { 
+            number1 = "-";
+        }
+        else if (number1 == "-") {
+            number1 = "";
+        }
+        else {
+            number1*= -1; 
+        }
+    }
+    else { 
+        if (number2 == "") {
+            number2 = "-";
+        }
+        else if (number2 == "-") {
+            number2 = "";
+        }
+        else {
+            number2*= -1; 
+        }
     }
 }
 
@@ -43,8 +86,20 @@ function divide(first, second) {
 
 function operate() {
     operated = true;
-    var realNumber1 = parseInt(number1);
-    var realNumber2 = parseInt(number2);
+    let realNumber1;
+    let realNumber2;
+    if (number1.includes(".")) {
+        realNumber1 = parseFloat(number1);
+    }
+    else {
+        realNumber1 = parseInt(number1);
+    }
+    if (number2.includes(".")) {
+        realNumber2 = parseFloat(number1);
+    }
+    else {
+        realNumber2 = parseInt(number2);
+    }
     if (operator == "+") {
         return add(realNumber1, realNumber2);
     }
@@ -58,19 +113,7 @@ function operate() {
         return divide(realNumber1, realNumber2);
     }
     alert("ERROR: operate() failed!");
-    return 0;
-}
-
-function giveCurrentNumber() {
-    if (operator == "" || operated == true) {
-        if (operated == true) {
-            resetCalculatorVariables();
-        }
-        return 1;
-    }
-    else {
-        return 2;
-    }
+    return NaN;
 }
 
 // ADDING BUTTONS TO CALCULATOR CONTAINER
@@ -94,47 +137,58 @@ const buttonListMap = new Map(Object.entries(buttonList));
 
 buttonListMap.forEach((element, name) => {
     element.textContent = name; // Objects, like the element node here, are always passed by reference which makes this mutation possible in a forEach method
-    element.addEventListener('click', (event) => {
-        switch (event.currentTarget.textContent) {
-            // Backtracking
-            case ("CE"):
-            case ("C"):
-            case ("<-"):
-                resetCalculatorVariables();
-                break;
-            // Digit adders
-            case ("1"):
-            case ("2"):
-            case ("3"):
-            case ("4"):
-            case ("5"):
-            case ("6"):
-            case ("7"):
-            case ("8"):
-            case ("9"):
-            case ("0"):
-                addDigitToNum(event.currentTarget.textContent, giveCurrentNumber());
-                break;
-            // Evaluation operators
-            case ("+"):
-            case ("-"):
-            case ("*"):
-            case ("/"):
-                operator = event.currentTarget.textContent;
-                break;
-            case ("="):
-                calculatorText.textContent+= " = " + operate();
-                return; //avoids reverting operated to true at the end
-            // Special number mutators
-            case ("+/-"): //strings can be operated on as numbers can except for when the + operator is used, so this case works as intended
-                if (giveCurrentNumber() == 1) { number1*= -1; }
-                else { number2*= -1; }
-                break;
-            case ("."):
-                // TODO
-                break;
-        }
+    switch (name) {
+        // Backtracking
+        case ("CE"):
+        case ("C"):
+        case ("<-"):
+            element.addEventListener('click', resetCalculatorVariables);
+            break;
+        // Number additions
+        case ("1"):
+        case ("2"):
+        case ("3"):
+        case ("4"):
+        case ("5"):
+        case ("6"):
+        case ("7"):
+        case ("8"):
+        case ("9"):
+        case ("0"):
+            element.addEventListener('click', (event) => { addToNum(event.currentTarget.textContent, giveCurrentNumber()) });
+            break;
+        // Evaluation operators
+        case ("+"):
+        case ("-"):
+        case ("*"):
+        case ("/"):
+            element.addEventListener('click', (event) => { operator = event.currentTarget.textContent });
+            break;
+        case ("="):
+            element.addEventListener('click', () => { 
+                result = operate();
+                if (result % 1 == 0) {
+                    calculatorResult.textContent = "= " + result; 
+                }
+                else {
+                    calculatorResult.textContent = "= " + parseFloat(result.toFixed(decimalLimit)); 
+                }
+            });
+            return; //avoids reverting operated to true for this element
+        // Special number mutators
+        case ("+/-"): //strings can be operated on as numbers can except for when the + operator is used, so this case works as intended
+            element.addEventListener('click', changeNumberSign);
+            break;
+        case ("."):
+            element.addEventListener('click', (event) => { 
+                if (!number1.includes(".")) {
+                    addToNum(event.currentTarget.textContent, giveCurrentNumber()) 
+                }
+            });
+            break;
+    }
+    element.addEventListener('click', () => {
         updateText();
         operated = false;
-    })
+    });
 });
